dfd478c22bbcde7a43b5dbaf78c68b744ed5d5f6
EAA1012 <eric.ali@ontariotechu.net>
2024-11-13 15:02:25 -0500

    Updated Add

--- routes/stock.js
+++ routes/stock.js
@@ -1,14 +1,19 @@
 // routes/product.js
-const express = require('express');
-const router = express.Router();
-const Stock = require('../model/Stock'); // Import the Stock model
+var express = require('express');
+var router = express.Router();
+let mongoose = require('mongoose');
+let Stock = require('../model/Stock');
+const stock = require('../model/Stock'); // Import the Stock model
 
 // GET Product page
 router.get('/', async (req, res) => {
-  try {
+  try 
+  {
     const stockItems = await Stock.find(); // Fetch all stock items from MongoDB
     res.render('stock', { title: 'Stock List', stockItems });
-  } catch (err) {
+  } 
+  catch (err) 
+  {
     console.error(err);
     res.status(500).send("Server Error");
   }
@@ -25,15 +30,15 @@ router.get('/add', async (req, res) => {
 
 router.post('/add',async(req,res,next)=>{
   try{
-      let newPart = Stock({
-          "description":req.body.description,
-          "item":req.body.item,
-          "price":req.body.price
-      });
-      Stock.create(newPart).then(()=>{
-          res.redirect('/stock');
-      });
-    }
+    let newPart = Stock({
+        "description":req.body.description,
+        "item":req.body.item,
+        "price":req.body.price
+    });
+    Stock.create(newPart).then(()=>{
+      res.redirect('/stock');
+      })
+  }
   catch(err)
   {
       console.error(err);
